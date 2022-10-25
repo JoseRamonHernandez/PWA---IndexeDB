7cb10ab2c9b4cfbf2b439903dea57b15dd36e903
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-24 22:00:00 -0600

    pouchDB

--- 08-pouchdb-todo/js/app.js
+++ 08-pouchdb-todo/js/app.js
@@ -1,30 +1,30 @@
 (function() {
+  "use strict";
 
-  'use strict';
 
-  if(navigator.serviceWorker)
-  {
+  if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js');
   }
 
+
   var ENTER_KEY = 13;
-  var newTodoDom = document.getElementById('new-todo');
-  var syncDom = document.getElementById('sync-wrapper');
+  var newTodoDom = document.getElementById("new-todo");
+  var syncDom = document.getElementById("sync-wrapper");
 
   // EDITING STARTS HERE (you dont need to edit anything above this line)
 
-  var db = new PouchDB('todos');
+  var db = new PouchDB("todos");
   var remoteCouch = false;
 
   db.changes({
-    since: 'now',
+    since: "now",
     live: true
-  }).on('change', showTodos);
+  }).on("change", showTodos);
 
   // We have to create a new todo document and enter it in the database
   function addTodo(text) {
 
-    if(trimmedText <= 0) return;
+    if ( text.length <= 0 ) return;
 
 
     var todo = {
@@ -33,33 +33,30 @@
       completed: false
     };
 
-   /* db.put(todo, function callback(err, result) {
-      if (!err) {
-        console.log('Successfully posted a todo!');
-      }
-    });*/
-
+    // db.put(todo, function callback(err, result) {
+    //   if (!err) {
+    //     console.log('Successfully posted a todo!');
+    //   }
+    // });
     db.put(todo)
-      .then( console.log('Insertado'))
-      .catch( console.log);
-
+      .then(console.log("Insertado"))
+      .catch(console.log);
   }
 
   // Show the current list of todos by reading them from the database
   function showTodos() {
-    /*db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-      redrawTodosUI(doc.rows);
-    });*/
+    // db.allDocs({ include_docs: true, descending: true }, function(err, doc) {
+    //   redrawTodosUI(doc.rows);
+    // });
 
-    db.allDocs({include_docs: true, descending: true})
-      .then( doc => {
-        redrawTodosUI(doc.rows); 
-      });
+    db.allDocs({ include_docs: true, descending: false }).then(doc => {
+      redrawTodosUI(doc.rows);
+    });
   }
 
   function checkboxChanged(todo, event) {
     todo.completed = event.target.checked;
-    db.put(todo);
+    db.put(todo); // .then( console.log('registro actualizado') );
   }
 
   // User pressed the delete button for a todo, delete it
@@ -70,36 +67,33 @@
   // The input box when editing a todo has blurred, we should save
   // the new title or delete the todo if the title is empty
   function todoBlurred(todo, event) {
+
     var trimmedText = event.target.value.trim();
 
-    
-    if(!trimmedText)
-    {
+    if (!trimmedText) {
       db.remove(todo);
-    }
-    else
-    {
+    } else {
       todo.title = trimmedText;
       db.put(todo);
     }
+
   }
 
   // Initialise a sync with the remote server
-  function sync() {
-  }
+  function sync() {}
 
   // EDITING STARTS HERE (you dont need to edit anything below this line)
 
   // There was some form or error syncing
   function syncError() {
-    syncDom.setAttribute('data-sync-state', 'error');
+    syncDom.setAttribute("data-sync-state", "error");
   }
 
   // User has double clicked a todo, display an input so they can edit the title
   function todoDblClicked(todo) {
-    var div = document.getElementById('li_' + todo._id);
-    var inputEditTodo = document.getElementById('input_' + todo._id);
-    div.className = 'editing';
+    var div = document.getElementById("li_" + todo._id);
+    var inputEditTodo = document.getElementById("input_" + todo._id);
+    div.className = "editing";
     inputEditTodo.focus();
   }
 
@@ -107,7 +101,7 @@
   // (or delete)
   function todoKeyPressed(todo, event) {
     if (event.keyCode === ENTER_KEY) {
-      var inputEditTodo = document.getElementById('input_' + todo._id);
+      var inputEditTodo = document.getElementById("input_" + todo._id);
       inputEditTodo.blur();
     }
   }
@@ -115,39 +109,39 @@
   // Given an object representing a todo, this will create a list item
   // to display it.
   function createTodoListItem(todo) {
-    var checkbox = document.createElement('input');
-    checkbox.className = 'toggle';
-    checkbox.type = 'checkbox';
-    checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
+    var checkbox = document.createElement("input");
+    checkbox.className = "toggle";
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", checkboxChanged.bind(this, todo));
 
-    var label = document.createElement('label');
-    label.appendChild( document.createTextNode(todo.title));
-    label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
+    var label = document.createElement("label");
+    label.appendChild(document.createTextNode(todo.title));
+    label.addEventListener("dblclick", todoDblClicked.bind(this, todo));
 
-    var deleteLink = document.createElement('button');
-    deleteLink.className = 'destroy';
-    deleteLink.addEventListener( 'click', deleteButtonPressed.bind(this, todo));
+    var deleteLink = document.createElement("button");
+    deleteLink.className = "destroy";
+    deleteLink.addEventListener("click", deleteButtonPressed.bind(this, todo));
 
-    var divDisplay = document.createElement('div');
-    divDisplay.className = 'view';
+    var divDisplay = document.createElement("div");
+    divDisplay.className = "view";
     divDisplay.appendChild(checkbox);
     divDisplay.appendChild(label);
     divDisplay.appendChild(deleteLink);
 
-    var inputEditTodo = document.createElement('input');
-    inputEditTodo.id = 'input_' + todo._id;
-    inputEditTodo.className = 'edit';
+    var inputEditTodo = document.createElement("input");
+    inputEditTodo.id = "input_" + todo._id;
+    inputEditTodo.className = "edit";
     inputEditTodo.value = todo.title;
-    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
-    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
+    inputEditTodo.addEventListener("keypress", todoKeyPressed.bind(this, todo));
+    inputEditTodo.addEventListener("blur", todoBlurred.bind(this, todo));
 
-    var li = document.createElement('li');
-    li.id = 'li_' + todo._id;
+    var li = document.createElement("li");
+    li.id = "li_" + todo._id;
     li.appendChild(divDisplay);
     li.appendChild(inputEditTodo);
 
     if (todo.completed) {
-      li.className += 'complete';
+      li.className += "complete";
       checkbox.checked = true;
     }
 
@@ -155,22 +149,22 @@
   }
 
   function redrawTodosUI(todos) {
-    var ul = document.getElementById('todo-list');
-    ul.innerHTML = '';
+    var ul = document.getElementById("todo-list");
+    ul.innerHTML = "";
     todos.forEach(function(todo) {
       ul.appendChild(createTodoListItem(todo.doc));
     });
   }
 
-  function newTodoKeyPressHandler( event ) {
+  function newTodoKeyPressHandler(event) {
     if (event.keyCode === ENTER_KEY) {
       addTodo(newTodoDom.value);
-      newTodoDom.value = '';
+      newTodoDom.value = "";
     }
   }
 
   function addEventListeners() {
-    newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
+    newTodoDom.addEventListener("keypress", newTodoKeyPressHandler, false);
   }
 
   addEventListeners();
@@ -179,5 +173,4 @@
   if (remoteCouch) {
     sync();
   }
-
 })();
